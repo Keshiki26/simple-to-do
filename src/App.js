ea375a3f5b31607e624b91cf55c2dd408bdabc98
Ujjwol Basnet <103056475@student.swin.edu.au>
2021-05-11 11:09:57 +1000

    Start

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import { Grid, Typography } from '@material-ui/core';
+import { Route } from 'react-router-dom';
+
+function App() {
+	return <Grid></Grid>;
+}
+
+export default App;
